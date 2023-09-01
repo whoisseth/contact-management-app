b957bdc553fe6d109b183bbbbede50c8ae675652
whoisseth <utkarshseth2019@gmail.com>
2023-09-02 00:15:28 +0530

    animation added

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,19 +1,18 @@
 /** @format */
 "use client";
 
-import Button from "@/components/Button";
 import Layout from "@/components/Layout";
-import { Modal } from "@/components/Modal";
 import { NoData } from "@/components/NoData";
-import React, { useState } from "react";
-import { BiErrorCircle } from "react-icons/bi";
+import React from "react";
 import { CreateContact_Modal } from "./CreateContact";
 import ContactCard from "@/components/ContactCard";
 import { useAtom } from "jotai";
 import { Contact, contactAtom } from "./store";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 export default function ContactPage() {
   const [contacts, setContacts] = useAtom(contactAtom);
+  const [animationParent] = useAutoAnimate();
 
   function handleDelete(contact: Contact) {
     const newContacts = contacts.filter((data) => data !== contact);
@@ -24,19 +23,21 @@ export default function ContactPage() {
     <Layout heading="Contact">
       <CreateContact_Modal className="mb-6" />
 
-      <main className="flex flex-col  justify-start gap-3  ">
+      <main
+        ref={animationParent}
+        className="flex flex-col  justify-start gap-3  "
+      >
         {contacts.length > 0 ? (
-          <>
-            {contacts.map((contact, index) => (
-              <ContactCard
-                handleDelete={() => handleDelete(contact)}
-                firstName={contact.firstName}
-                lastName={contact.lastName}
-                status={contact.status}
-                key={index}
-              />
-            ))}
-          </>
+          contacts.map((contact, index) => (
+            <ContactCard
+              contactData={contact}
+              handleDelete={() => handleDelete(contact)}
+              firstName={contact.firstName}
+              lastName={contact.lastName}
+              status={contact.status}
+              key={index}
+            />
+          ))
         ) : (
           <NoData />
         )}
